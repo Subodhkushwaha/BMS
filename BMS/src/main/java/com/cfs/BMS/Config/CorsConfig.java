6ab3c487a0a6ca9939a17d944dde00b05bf2a928
Subodh Kushwaha <subodhkushwaha36@gmail.com>
2026-07-04 16:25:42 +0530

    Update CORS configuration to use CorsFilter
    Refactored CORS configuration to use CorsFilter and allowed specific origins.

--- BMS/src/main/java/com/cfs/BMS/Config/CorsConfig.java
+++ BMS/src/main/java/com/cfs/BMS/Config/CorsConfig.java
@@ -1,25 +1,31 @@
-package com.cfs.BMS.config;
+package com.example.BMS.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig  {
+public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigMapping() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
-            };
-        };
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://127.0.0.1:5500");
+        config.addAllowedOrigin("http://localhost:5500");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
-
-
 }
